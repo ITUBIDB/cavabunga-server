ca6f7cf3a83474d03a8c4c089957550d9af010b8
Doğukan Çelik <dogukan.celik@itu.edu.tr>
2017-12-21 19:30:47 +0300

    IcalStorageService

--- src/main/java/edu/itu/cavabunga/caldav/CaldavController.java
+++ src/main/java/edu/itu/cavabunga/caldav/CaldavController.java
@@ -2,7 +2,6 @@ package edu.itu.cavabunga.caldav;
 
 import edu.itu.cavabunga.core.IcalStorageService;
 import edu.itu.cavabunga.core.ParticipantStorageService;
-import edu.itu.cavabunga.core.entity.Component;
 import edu.itu.cavabunga.core.entity.Participant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @RequestMapping(path="/calendar")
@@ -21,24 +18,6 @@ public class CaldavController {
 
     @Autowired
     private ParticipantStorageService participantStorageService;
-
-    @GetMapping(path="/add")
-    public @ResponseBody String test(){
-        Participant participant = participantStorageService.createParticipant("ali veli");
-        icalStorageService.createCalendar();
-        return "tamam";
-    }
-
-    @GetMapping(path="/get/i")
-    public @ResponseBody Component getCalendarById(@RequestParam Long id){
-        return icalStorageService.getCalendarById(id);
-    }
-
-    @GetMapping(path="/get/u")
-    public @ResponseBody
-    List<Component> getCalendarByUser(@RequestParam String user_name){
-        return icalStorageService.getCalendarByOwner(user_name);
-    }
 
     @GetMapping(path="/participant/u")
     public @ResponseBody Participant getParticipantByUserName(@RequestParam String user_name){
@@ -50,22 +29,5 @@ public class CaldavController {
         return participantStorageService.getParticipantByUuid(uuid);
     }
 
-    @GetMapping(path = "/")
-    public @ResponseBody Iterable<Component> createNewUser(){
-        return icalStorageService.getAllCalendars();
-    }
-    /*
-    public @ResponseBody String addNewCalendar(){
-        componentFactory.createComponent(ComponentType.CALENDAR);
-        return "kayit tamamlandi";
-    }
-
-
-
-
-    @GetMapping(path="/showcalendars")
-    public @ResponseBody Iterable<Component> getAllComponents(){
-        return componentRepository.findAll();
-    } */
 
 }
--- src/main/java/edu/itu/cavabunga/core/IcalStorageService.java
+++ src/main/java/edu/itu/cavabunga/core/IcalStorageService.java
@@ -2,11 +2,16 @@ package edu.itu.cavabunga.core;
 
 import edu.itu.cavabunga.core.entity.Component;
 import edu.itu.cavabunga.core.entity.Participant;
-import edu.itu.cavabunga.core.entity.component.Calendar;
+import edu.itu.cavabunga.core.entity.Property;
 import edu.itu.cavabunga.core.entity.component.ComponentType;
+import edu.itu.cavabunga.core.entity.property.PropertyType;
 import edu.itu.cavabunga.core.exception.IcalStorageException;
 import edu.itu.cavabunga.core.factory.ComponentFactory;
+import edu.itu.cavabunga.core.factory.ParameterFactory;
+import edu.itu.cavabunga.core.factory.PropertyFactory;
 import edu.itu.cavabunga.core.repository.ComponentRepository;
+import edu.itu.cavabunga.core.repository.ParameterRepository;
+import edu.itu.cavabunga.core.repository.PropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,43 +20,25 @@ import java.util.List;
 @Service
 public class IcalStorageService {
     @Autowired
+    public ParticipantStorageService participantStorageService;
+
+    @Autowired
     public ComponentFactory componentFactory;
 
     @Autowired
     public ComponentRepository componentRepository;
 
     @Autowired
-    public ParticipantStorageService participantStorageService;
+    public PropertyFactory propertyFactory;
 
-    public void createCalendar(){
-        componentRepository.save(componentFactory.createComponent(ComponentType.CALENDAR));
-    }
+    @Autowired
+    public PropertyRepository propertyRepository;
 
-    public Component createCalendar(Participant participant){
-        Component calendar = componentFactory.createComponent(ComponentType.CALENDAR);
-        calendar.setOwner(participant);
-        componentRepository.save(calendar);
-        return calendar;
-    }
+    @Autowired
+    public ParameterFactory parameterFactory;
 
-    public void saveCalendar(Calendar calendar){
-        calendar.validate();
-        componentRepository.save(calendar);
-    }
-
-    public Component getCalendarById(Long id){
-        return componentRepository.findById(id);
-    }
-
-    public List<Component> getCalendarByOwner(String user_name){
-        return componentRepository.findByOwnerAndComponentToComponentMapIsNull(participantStorageService.getParticipantByUserName(user_name));
-    }
-
-    public Iterable<Component> getAllCalendars(){
-        return componentRepository.findAll();
-    }
-
-    //-------------------------------------
+    @Autowired
+    public ParameterRepository parameterRepository;
 
     public Component createComponent(ComponentType componentType) throws IcalStorageException{
         Component result = componentFactory.createComponent(componentType);
@@ -62,7 +49,7 @@ public class IcalStorageService {
         return result;
     }
 
-    public Component createComponentForParticipant(ComponentType componentType, Participant participant){
+    public Component createComponentForParticipant(ComponentType componentType, Participant participant) throws IcalStorageException{
         Component result = componentFactory.createComponent(componentType);
         if(participant == null){
             throw new IcalStorageException("Cant reach participant object in IcalStorageService");
@@ -76,7 +63,7 @@ public class IcalStorageService {
         return result;
     }
 
-    public Component getComponentById(Long id){
+    public Component getComponentById(Long id) throws IcalStorageException{
         Component result = componentRepository.findOne(id);
         if(result == null){
             throw new IcalStorageException("Cant find component with given id: " + id);
@@ -85,7 +72,7 @@ public class IcalStorageService {
         return result;
     }
 
-    public List<Component> getComponentByParticipant(Participant participant){
+    public List<Component> getComponentByParticipant(Participant participant) throws IcalStorageException{
         List<Component> result = componentRepository.findByOwner(participant);
         if(participant == null){
             throw new IcalStorageException("Cant reach participant object in IcalStorageService");
@@ -99,7 +86,7 @@ public class IcalStorageService {
         return result;
     }
 
-    public List<Component> getSpecificComponentsOfParticipant(ComponentType componentType, Participant participant){
+    public List<Component> getSpecificComponentsOfParticipant(ComponentType componentType, Participant participant) throws IcalStorageException{
         if(componentType == null || participant == null){
             throw new IcalStorageException("Cant reach componentType or participant in IcalStorageService");
         }
@@ -111,5 +98,32 @@ public class IcalStorageService {
         return result;
     }
 
+    public void saveComponent(Component component) throws IcalStorageException{
+        componentRepository.save(component);
+    }
 
+    public Property createProperty(PropertyType propertyType) throws IcalStorageException{
+        Property result = propertyFactory.createProperty(propertyType);
+        if (result == null){
+            throw new IcalStorageException("Object cant crated in IcalStorageService: " + propertyType.toString());
+        }
+        return result;
+    }
+
+    public Property getPropertyById(Long id) throws IcalStorageException{
+        Property result = propertyRepository.findOne(id);
+        if(result == null){
+            throw new IcalStorageException("Cant find object in IcalStroageService");
+        }
+        return result;
+    }
+
+    public List<Property> getPropertyByComponent(Component component){
+        List<Property> result = propertyRepository.findByComponentToPropertyMap(component);
+        if(result == null || result.size() == 0){
+            throw new IcalStorageException("Cant get object from reposityory in IcalStorageService");
+        }
+
+        return result;
+    }
 }
