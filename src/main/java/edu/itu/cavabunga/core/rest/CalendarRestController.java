cbab5d7e944cf94ed306ede77d6e468dbdc1d909
Doğukan ÇELİK <dogukan.celik@itu.edu.tr>
2018-01-22 19:00:29 +0300

    moving rest into core

--- src/main/java/edu/itu/cavabunga/core/rest/CalendarRestController.java
+++ src/main/java/edu/itu/cavabunga/core/rest/CalendarRestController.java
@@ -1,11 +1,10 @@
-package edu.itu.cavabunga.rest;
+package edu.itu.cavabunga.core.rest;
 
 import edu.itu.cavabunga.core.IcalStorageService;
 import edu.itu.cavabunga.core.ParticipantStorageService;
 import edu.itu.cavabunga.core.entity.Component;
 import edu.itu.cavabunga.core.entity.Participant;
 import edu.itu.cavabunga.core.entity.Property;
-import edu.itu.cavabunga.core.entity.component.Calendar;
 import edu.itu.cavabunga.core.entity.component.ComponentType;
 import edu.itu.cavabunga.core.entity.property.PropertyType;
 import org.springframework.beans.factory.annotation.Autowired;
