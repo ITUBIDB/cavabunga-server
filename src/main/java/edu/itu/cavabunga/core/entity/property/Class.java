e0060a98b8ebbee2be43b94ee5e4d0a1172cc75f
Doğukan ÇELİK <dogukan.celik@itu.edu.tr>
2018-05-07 07:07:39 +0300

    Implementin validation overide methods for properties.

--- src/main/java/edu/itu/cavabunga/core/entity/property/Class.java
+++ src/main/java/edu/itu/cavabunga/core/entity/property/Class.java
@@ -1,9 +1,34 @@
 package edu.itu.cavabunga.core.entity.property;
 
+import edu.itu.cavabunga.core.entity.Parameter;
 import edu.itu.cavabunga.core.entity.Property;
+import edu.itu.cavabunga.core.entity.component.Alarm;
+import edu.itu.cavabunga.exception.Validation;
 
 import javax.persistence.Entity;
 
 @Entity
 public class Class extends Property {
+    @Override
+    public void validate(){
+        if(this.getValue().trim() != ""){
+            throw new Validation("CLASS property cannot be empty");
+        }
+
+        if(this.getValue() != "PUBLIC" &&
+                this.getValue() != "PRIVATE" &&
+                this.getValue() != "CONFIDENTIAL"){
+            throw new Validation("CLASS value is different from acceptable value range: " + this.getValue());
+        }
+
+          if(!this.getParameters().isEmpty()){
+            for(Parameter pr : this.getParameters()){
+                try {
+                    pr.validate();
+                }catch (Exception e){
+                    throw new Validation("CLASS parameter validation failed: " + pr.getValue());
+                }
+          }
+        }
+    }
 }
--- src/main/java/edu/itu/cavabunga/core/entity/property/Completed.java
+++ src/main/java/edu/itu/cavabunga/core/entity/property/Completed.java
@@ -1,9 +1,33 @@
 package edu.itu.cavabunga.core.entity.property;
 
+import edu.itu.cavabunga.core.entity.Parameter;
 import edu.itu.cavabunga.core.entity.Property;
+import edu.itu.cavabunga.core.entity.component.Alarm;
+import edu.itu.cavabunga.core.entity.component.Todo;
+import edu.itu.cavabunga.exception.Validation;
 
 import javax.persistence.Entity;
 
 @Entity
 public class Completed extends Property {
+    @Override
+    public void validate(){
+        if(this.getValue().trim() != ""){
+            throw new Validation("COMPLETED property cannot be empty");
+        }
+
+        if(!(this.getComponent() instanceof Todo)){
+            throw new Validation("COMPLETED property can only be use in Todo component");
+        }
+
+        if(!this.getParameters().isEmpty()){
+            for(Parameter pr : this.getParameters()){
+                try {
+                    pr.validate();
+                }catch (Exception e){
+                    throw new Validation("COMPLETED parameter validation failed: " + pr.getValue());
+                }
+            }
+        }
+    }
 }
